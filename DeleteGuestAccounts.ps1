1826cb2530eddc322a0469a7ecd75d7d67a93e5e
Matthew Novitsch <61195587+mattnovitsch@users.noreply.github.com>
2021-09-03 14:58:31 -0400

    Add files via upload

---
+++ DeleteGuestAccounts.ps1
@@ -0,0 +1,15 @@
+﻿Connect-MsolService
+$Results = @();
+$Users = Get-Content C:\HWID\Remove_guest.csv
+
+$licensedUsers = Get-MsolUser -all | Where-Object { $_.UserType -eq "Guest" } | Select  UserPrincipalName,@{Name="AlternateEmailAddresses";Expression={$_.AlternateEmailAddresses}}
+$licensedUsers | ForEach-Object {
+$AlternateEmailAddresses = $_.AlternateEmailAddresses
+$UPN = $_.UserPrincipalName
+    If ($Users -eq "$AlternateEmailAddresses")
+    {
+        $Results += $UPN
+        #Remove-MsolUser -UserPrincipalName $UPN -force
+    }
+}
+$Results
