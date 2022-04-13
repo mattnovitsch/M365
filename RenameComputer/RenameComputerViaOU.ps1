bec688ba3e1a71a99bf867e192204f37628dcfb0
Matthew Novitsch <61195587+mattnovitsch@users.noreply.github.com>
2022-04-13 07:59:21 -0400

    Create RenameComputerViaOU.ps1

---
+++ RenameComputer/RenameComputerViaOU.ps1
@@ -0,0 +1,12 @@
+﻿#Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0 -Online
+#Import-Module -Name ActiveDirectory
+$ADComputerinfo = get-adcomputer -identity $env:COMPUTERNAME -Properties *
+$ComputerINT = $ADComputerinfo.ObjectCategory.IndexOf(",")
+$ADComputerinfo.ObjectCategory.substring(3, $ComputerINT-3)
+
+If(($ADComputerinfo.ObjectCategory.substring(3, $ComputerINT-3)) -eq "Computer" )
+{
+    $Computername = 'DC01' + $SerialNumber.Serialnumber
+    $Computername
+    #Rename-Computer -NewName $Computername
+} 
